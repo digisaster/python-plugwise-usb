a9dc316b000fc1c7370a06e2adb3c48a902826cd
Digisaster <github@digisaster.com>
2025-02-18 21:39:54 +0100

    Update circle.py
    Minimal sleep to prevent high CPU

--- plugwise_usb/nodes/circle.py
+++ plugwise_usb/nodes/circle.py
@@ -474,7 +474,7 @@ class PlugwiseCircle(PlugwiseBaseNode):
             missing_addresses = sorted(missing_addresses, reverse=True)
             for address in missing_addresses:
                 await self.energy_log_update(address)
-
+                await ayncio.sleep(0.05)
         if self._cache_enabled:
             await self._energy_log_records_save_to_cache()
 
